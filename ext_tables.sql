21ad3f95117bf45fb36b2fece48dd65ff448a758
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-24 20:26:05 +0000

    0.5.1 +++ Multiple manager for departments
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@66328 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -406,7 +406,7 @@ CREATE TABLE tx_org_department (
   shortcut tinytext,
   tx_org_departmentcat tinytext,
   tx_org_headquarters int(11) unsigned DEFAULT '0' NOT NULL,
-  manager int(11) unsigned DEFAULT '0' NOT NULL,
+  manager tinytext,
   telephone tinytext NOT NULL,
   fax tinytext NOT NULL,
   email tinytext NOT NULL,
