7c3a84d801e331ef609cac125a694eeaa4f6281b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-27 19:03:02 +0000

    3.3.0 +++ #50490
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@78501 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ tsConfig/page/tx_linkhandler/default/tx_org_cal.php
@@ -0,0 +1,59 @@
+<?php
+
+if (!defined ('TYPO3_MODE'))
+{
+  die ('Access denied.');
+}
+
+  // Should tx_org_cal included?
+$includeIt = $confArr['linkhandler.']['tx_org_cal'];
+
+  // RETURN : tx_org_cal should not include
+switch( $includeIt ) 
+{
+  case( 'No' ):
+    return;
+    break;
+  case( 'Yes (recommended)' ):
+  default:
+    // follow the workflow
+    break;
+}
+  // RETURN : tx_org_cal should not include
+
+
+  // Init tsConfig
+$tsConfig = '
+
+mod.dwildt.test = 1
+mod.tx_linkhandler {
+  tx_org_cal {
+    label       = Org: Kalender
+    listTables  = tx_org_cal
+    %onlyPids%
+    onlyPids    = 1
+  }
+}
+
+RTE.default.tx_linkhandler.tx_org_cal < mod.tx_linkhandler.tx_org_cal
+';
+
+$onlyPids = $confArr['linkhandler.']['tx_org_cal.']['onlyPids'];
+
+switch( true )
+{
+  case( empty ( $onlyPids ) ):
+    $onlyPids = 'onlyPids    = ' . $onlyPids;
+    break;
+  case( empty ( $onlyPids ) ):
+  default;
+    $onlyPids = null;
+    break;  
+}
+
+$tsConfig = str_replace( '%onlyPids%', $onlyPids, $tsConfig );
+t3lib_extMgm::addPageTSConfig( $tsConfig );
+
+return;
+
+?>
