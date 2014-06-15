60c72749059d87bc47c2c81ea893a09ffdd394ef
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-06-15 14:45:40 +0000

    5.0.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@85627 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables/tca/tx_org_headquarters.php
+++ ext_tables/tca/tx_org_headquarters.php
@@ -14,8 +14,6 @@ if (!defined('TYPO3_MODE'))
 // headquarters
 // headquarterscat
 // headquartersheadquarters
-// department
-// departmentcat
 //
 // headquarters //////////////////////////////////////////////////////////////////
 $TCA['tx_org_headquarters'] = array(
@@ -114,49 +112,4 @@ $TCA['tx_org_headquarterscat'] = array(
   )
 );
 // headquarterscat ////////////////////////////////////////////////////////////////////
-// org /////////////////////////////////////////////////////////////////////
-$TCA['tx_org_department'] = array(
-  'ctrl' => array(
-    'title' => 'LLL:EXT:org/tca/locallang/tx_org_headquarters.xml:tx_org_department',
-    'label' => 'title',
-    'tstamp' => 'tstamp',
-    'crdate' => 'crdate',
-    'cruser_id' => 'cruser_id',
-    'languageField' => 'sys_language_uid',
-    'transOrigPointerField' => 'l10n_parent',
-    'transOrigDiffSourceField' => 'l10n_diffsource',
-    'sortby' => 'sorting',
-    'delete' => 'deleted',
-    'enablecolumns' => array(
-      'disabled' => 'hidden',
-      'fe_group' => 'fe_group',
-    ),
-    'dividers2tabs' => true,
-    'hideAtCopy' => true,
-    'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
-    'thumbnail' => 'image',
-    'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon/department.gif',
-  ),
-);
-// org /////////////////////////////////////////////////////////////////////
-// departmentcat ////////////////////////////////////////////////////////////////////
-$TCA['tx_org_departmentcat'] = array(
-  'ctrl' => array(
-    'title' => 'LLL:EXT:org/tca/locallang/tx_org_headquarters.xml:tx_org_departmentcat',
-    'label' => 'title',
-    'tstamp' => 'tstamp',
-    'crdate' => 'crdate',
-    'sortby' => 'sorting',
-    'delete' => 'deleted',
-    'enablecolumns' => array(
-      'disabled' => 'hidden',
-    ),
-    'hideAtCopy' => false,
-    'dividers2tabs' => true,
-    'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY) . 'tca.php',
-    'thumbnail' => 'image',
-    'iconfile' => t3lib_extMgm::extRelPath($_EXTKEY) . 'ext_icon/departmentcat.gif',
-  )
-);
-// departmentcat ////////////////////////////////////////////////////////////////////
 ?>
