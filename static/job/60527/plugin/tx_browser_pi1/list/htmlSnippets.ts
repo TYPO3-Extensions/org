7eb9fafcdb7aef231f960a8ff91118ba4e367adf
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-09-18 11:35:13 +0000

    6.0.0 +++ #61696
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@86349 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ static/job/60527/plugin/tx_browser_pi1/list/htmlSnippets.ts
@@ -0,0 +1,25 @@
+plugin.tx_browser_pi1 {
+  views {
+    list {
+      60527 {
+        htmlSnippets =
+        htmlSnippets {
+          subparts {
+            listview = TEXT
+            listview {
+              value (
+                <div class="content">
+                  <!-- ###LISTBODY### begin --><!-- ###LISTBODYITEM### begin -->
+                  <div class="tx_org_job tx_org_job-record record">
+                    ###TX_ORG_JOB.TITLE###
+                  </div>
+                  <!-- ###LISTBODYITEM### end --><!-- ###LISTBODY### end -->
+                </div> <!-- /listview -->
+)
+            }
+          }
+        }
+      }
+    }
+  }
+}
