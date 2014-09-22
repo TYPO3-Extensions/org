dad2bbfaf4cf4bb7fae879eea08255e7bf203194
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-09-22 09:53:37 +0000

    6.0.0 +++ #61793
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@86384 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ static/news/401/tx_browser_pi1/single/marker.ts
@@ -0,0 +1,22 @@
+plugin.tx_browser_pi1 {
+  marker {
+    fdTmplMainSingleColLeftSmall {
+      value = 12
+    }
+    fdTmplMainSingleColLeftMedium {
+      value = 12
+    }
+    fdTmplMainSingleColLeftLarge {
+      value = 3
+    }
+    fdTmplMainSingleColRightSmall {
+      value = 12
+    }
+    fdTmplMainSingleColRightMedium {
+      value = 12
+    }
+    fdTmplMainSingleColRightLarge {
+      value = 9
+    }
+  }
+}
