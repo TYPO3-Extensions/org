2816281eec075477004bebe6f4396c0327d4ba0d
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-09-23 15:12:42 +0000

    6.0.0 +++ #61819
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@86398 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ static/calendar/201/tx_browser_pi1/list/htmlSnippets.ts
@@ -0,0 +1,41 @@
+plugin.tx_browser_pi1 {
+  views {
+    list {
+      201 {
+        htmlSnippets =
+        htmlSnippets {
+          marker {
+            filter = TEXT
+            filter {
+              value (
+              <div class="filter">
+                ###RADIALSEARCH###
+                ###TX_ORG_HEADQUARTERSCAT.TITLE###
+              </div>
+)
+            }
+          }
+          subparts {
+              // For foundation main_02.html
+            //searchform < plugin.tx_browser_pi1.displayList.master_templates.subparts.listview.searchform.simpleMapOrderFilter
+              // For foundation main_01.html
+            searchform < plugin.tx_browser_pi1.displayList.master_templates.subparts.listview.searchform.simple
+            listview = TEXT
+            listview {
+              value (
+            <div id="c###TT_CONTENT.UID###-listview-###MODE###" class="columns listview listview-content listview-###MODE### listview-content-###MODE###">
+              <!-- ###LISTBODY### begin --><!-- ###LISTBODYITEM### begin -->
+              <div class="record###TX_ORG_HEADQUARTERS.CRDATE###">
+                ###TX_ORG_HEADQUARTERS.DELETED###
+                ###TX_ORG_HEADQUARTERS.TITLE###
+              </div>
+              <div class="cleaner">&nbsp;</div><!-- ###LISTBODYITEM### end --><!-- ###LISTBODY### end -->
+              </div> <!-- /listview -->
+)
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ static/headquarters/501/tx_browser_pi1/list/filter.ts
@@ -0,0 +1,37 @@
+plugin.tx_browser_pi1 {
+  views {
+    list {
+      501 {
+        filter {
+          radialsearch < plugin.tx_browser_pi1.displayList.master_templates.radialsearch
+          tx_org_headquarterscat {
+              // #43641, dwildt, 1-, 1+
+            //title < plugin.tx_browser_pi1.displayList.master_templates.selectbox
+            title < plugin.tx_browser_pi1.displayList.master_templates.treeview
+            title {
+              count_hits = 0
+              treeview {
+                enabled {
+                  value = 1
+                }
+                html_id {
+                  value = {$plugin.tx_browser_pi1.jQuery.plugin.jstree.selector_01}
+                }
+              }
+              first_item {
+                cObject {
+                  20 {
+                    data = LLL:EXT:org/locallang_db.xml:filter_phrase.headquarterscat
+                  }
+                }
+              }
+                // #43641, dwildt, 1-, 1+
+              //wrap = <span class="selectbox">|</span>
+              wrap = <div class="category_menu category_menu_treeview">|</div>
+            }
+          }
+        }
+      }
+    }
+  }
+}
