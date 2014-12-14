6d01e94a16f03d8fce280a0048958ad930510656
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-14 22:22:07 +0000

    6.0.7 +++ 101
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@87433 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/staff/101/tx_browser_pi1/list/sql.ts
+++ Configuration/TypoScript/staff/101/tx_browser_pi1/list/sql.ts
@@ -7,16 +7,19 @@ plugin.tx_browser_pi1 {
           tx_org_staff.bodytext,
           tx_org_staff.image,
           tx_org_staff.imageseo,
+          tx_org_staff.name_first,
+          tx_org_staff.name_last,
           tx_org_staff.page,
           tx_org_staff.seo_description,
           tx_org_staff.seo_keywords,
           tx_org_staff.type,
+          tx_org_staff.uid,
           tx_org_staff.url,
           tx_org_staff.vita,
-          tx_org_staffgroup.title
+          tx_org_staffgroup.uid
         )
         orderBy (
-          tx_org_staff.title, tx_org_staffgroup.title
+          tx_org_staff.name_last, tx_org_staff.title, tx_org_staffgroup.title
         )
         // Workaround: Without it i.e. the filename in tx_org_headquarters.image would get a typolink!
         csvLinkToSingleView = tx_org_job.title
--- Configuration/TypoScript/staff/101/tx_browser_pi1/list/tableFields.ts
+++ Configuration/TypoScript/staff/101/tx_browser_pi1/list/tableFields.ts
@@ -27,7 +27,51 @@ plugin.tx_browser_pi1 {
             20 = COA
             20 {
                 // name
-              10 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.header.0
+              //10 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.header.0
+              10 = COA
+              10 {
+                wrap = <div class="tx_org_staff_name">|</div>
+                  // name_first, name_last
+                  // title (linked)
+                10 = CASE
+                10 {
+                  key {
+                    field = {$plugin.tx_browser_pi1.templates.listview.url.0.key}
+                  }
+                  default = COA
+                  default {
+                    10 = TEXT
+                    10 {
+                      field = tx_org_staff.name_first
+                      noTrimWrap = || |
+                    }
+                    20 = TEXT
+                    20 {
+                      field = tx_org_staff.name_last
+                    }
+                    stdWrap {
+                      typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.default
+                    }
+                  }
+                  notype = TEXT
+                  notype {
+                    stdWrap >
+                  }
+                  page < .default
+                  page {
+                    stdWrap {
+                      typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.page
+                    }
+                  }
+                  url < .page
+                  url {
+                    stdWrap {
+                      typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.url
+                    }
+                  }
+                }
+              }
+
                 // bodytext || vita
               20 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.0
               wrap = <div class="columns small-12 medium-12 large-10">|</div>
