925a5993fc50fd31f58065cf46b9de4fc720f751
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-14 22:33:56 +0000

    6.0.7 +++ 101
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@87435 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/staff/101/tx_browser_pi1/list/tableFields.ts
+++ Configuration/TypoScript/staff/101/tx_browser_pi1/list/tableFields.ts
@@ -75,6 +75,57 @@ plugin.tx_browser_pi1 {
 
                 // bodytext || vita
               20 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.0
+                // for debugging only
+              30 = TEXT
+              30 {
+                field = tx_org_staffgroup.uid
+                noTrimWrap = |<div>staffgroup: |</div> |
+              }
+              30 >
+                // tx_org_staffgroup
+              31 = COA
+              31 {
+                  // if is true tx_org_staffgroup.uid
+                if =
+                if {
+                  isTrue {
+                    field = tx_org_staffgroup.uid
+                  }
+                }
+                  // div box
+                wrap = <div class="tx_org_staffgroup">|</div>
+                20 = CONTENT
+                20 {
+                  table = tx_org_staffgroup
+                  select {
+                    pidInList = {$plugin.org.sysfolder.staff}
+                    //selectFields = tx_org_staffgroup.title
+                    join = tx_org_mm_all ON tx_org_mm_all.uid_foreign = tx_org_staffgroup.uid
+                    where {
+                      field = tx_org_staff.uid
+                      noTrimWrap = |tx_org_mm_all.uid_local = | AND tx_org_mm_all.table_local = 'tx_org_staff' AND tx_org_mm_all.table_foreign = 'tx_org_staffgroup'|
+                    }
+                    orderBy = tx_org_staffgroup.title
+                  }
+                    // tx_org_staff.title
+                  renderObj = TEXT
+                  renderObj {
+                    field = title
+                    //noTrimWrap = |<span class="item">|</span>|
+                    wrap = |###POINT###
+                  }
+                  stdWrap {
+                    split {
+                      token = ###POINT###
+                      cObjNum = 1 |*| 1 |*| 2 || 2
+                      1.current = 1
+                      1.noTrimWrap = ||, |
+                      2.current = 1
+                      2.noTrimWrap = |||
+                    }
+                  }
+                }
+              }
               wrap = <div class="columns small-12 medium-12 large-10">|</div>
             }
             wrap = <div class="row">|</div>
