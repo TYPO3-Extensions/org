79645b3ce6e81cff9e76478454d2ea08f734c886
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-26 20:13:15 +0000

    6.0.7 +++ 501
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@87820 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/headquarters/501/tx_browser_pi1/single/tableFields/tx_org_headquarters.ts
+++ Configuration/TypoScript/headquarters/501/tx_browser_pi1/single/tableFields/tx_org_headquarters.ts
@@ -189,14 +189,14 @@ plugin.tx_browser_pi1 {
                           renderObj = CASE
                           renderObj {
                             key {
-                              field = {$plugin.tx_browser_pi1.templates.listview.url.0.key}
+                              field = {$plugin.tx_browser_pi1.templates.listview.url.2.key}
                             }
                               // link to detail view
                             default = TEXT
                             default {
                               field = title
                               wrap = |###POINT###
-                              typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.default
+                              typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.default
                             }
                               // no link
                             notype = TEXT
@@ -206,12 +206,12 @@ plugin.tx_browser_pi1 {
                               // link to internal page
                             page < .default
                             page {
-                              typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.page
+                              typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.page
                             }
                               // link to external url
                             url < .page
                             url {
-                              typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.url
+                              typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.url
                             }
                           }
                         }
