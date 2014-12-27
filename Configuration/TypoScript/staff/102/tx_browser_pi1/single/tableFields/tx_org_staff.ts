ae5fec7990774667a7156a4cc5288db41e3c25e0
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-27 01:02:10 +0000

    6.0.7 +++ 511
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@87847 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/staff/102/tx_browser_pi1/single/tableFields/tx_org_staff.ts
+++ Configuration/TypoScript/staff/102/tx_browser_pi1/single/tableFields/tx_org_staff.ts
@@ -32,145 +32,170 @@ plugin.tx_browser_pi1 {
                     // contact: header, name, contact_email, contact_phone, contact_fax, contact_skype, contact_url,
                   30 = COA
                   30 {
-                      // column: image, header, title, steet, zip city, url
-                    10 = COA
-                    10 {
-                      wrap = <ul class="vcard tx_org_staff">|</ul><!-- vcard -->
-                        // header
-                      10 = COA
-                      10 {
-                        wrap = <li class="header">|</li>
-                          // title, if title (name)
-                        10 = TEXT
-                        10 {
-                          value = Contact data
-                          lang {
-                            de = Kontakt
-                            en = Contact data
+                    if {
+                      isTrue {
+                        stdWrap {
+                          cObject = COA
+                          cObject {
+                            10 = TEXT
+                            10 {
+                              field = tx_org_staff.contact_email
+                            }
+                            20 = TEXT
+                            20 {
+                              field = tx_org_staff.contact_fax
+                            }
+                            30 = TEXT
+                            30 {
+                              field = tx_org_staff.contact_phone
+                            }
+                            40 = TEXT
+                            40 {
+                              field = tx_org_staff.contact_skype
+                            }
+                            50 = TEXT
+                            50 {
+                              field = tx_org_staff.contact_url
+                            }
                           }
                         }
                       }
-                        // contact_email
-                      30 = COA
-                      30 {
-                        if {
-                          isTrue {
+                    }
+                    wrap = <ul class="vcard tx_org_staff">|</ul><!-- vcard -->
+                      // header
+                    10 = COA
+                    10 {
+                      wrap = <li class="header">|</li>
+                        // title, if title (name)
+                      10 = TEXT
+                      10 {
+                        value = Contact data
+                        lang {
+                          de = Kontakt
+                          en = Contact data
+                        }
+                      }
+                    }
+                      // contact_email
+                    30 = COA
+                    30 {
+                      if {
+                        isTrue {
+                          field = tx_org_staff.contact_email
+                        }
+                      }
+                      10 = TEXT
+                      10 {
+                        value = e-mail
+                        lang {
+                          de = E-Mail
+                          en = e-mail
+                        }
+                        noTrimWrap = ||: |
+                      }
+                      20 = TEXT
+                      20 {
+                        typolink {
+                          parameter {
                             field = tx_org_staff.contact_email
                           }
                         }
-                        10 = TEXT
-                        10 {
-                          value = e-mail
-                          lang {
-                            de = E-Mail
-                            en = e-mail
-                          }
-                          noTrimWrap = ||: |
-                        }
-                        20 = TEXT
-                        20 {
-                          typolink {
-                            parameter {
-                              field = tx_org_staff.contact_email
-                            }
-                          }
-                        }
-                        wrap = <li class="contact_email">|</li>
                       }
-                        // contact_phone
-                      40 = COA
-                      40 {
-                        if {
-                          isTrue {
-                            field = tx_org_staff.contact_phone
-                          }
-                        }
-                        wrap = <li class="contact_phone">|</li>
-                        10 = TEXT
-                        10 {
-                          value = phone
-                          lang {
-                            de = Tel.
-                            en = phone
-                          }
-                          noTrimWrap = ||: |
-                        }
-                        20 = TEXT
-                        20 {
+                      wrap = <li class="contact_email">|</li>
+                    }
+                      // contact_phone
+                    40 = COA
+                    40 {
+                      if {
+                        isTrue {
                           field = tx_org_staff.contact_phone
-                          required = 1
                         }
                       }
-                        // contact_fax
-                      50 = COA
-                      50 {
-                        if {
-                          isTrue {
-                            field = tx_org_staff.contact_fax
-                          }
+                      wrap = <li class="contact_phone">|</li>
+                      10 = TEXT
+                      10 {
+                        value = phone
+                        lang {
+                          de = Tel.
+                          en = phone
                         }
-                        wrap = <li class="contact_fax">|</li>
-                        10 = TEXT
-                        10 {
-                          value = fax
-                          lang {
-                            de = Fax
-                            en = fax
-                          }
-                          noTrimWrap = ||: |
-                        }
-                        20 = TEXT
-                        20 {
+                        noTrimWrap = ||: |
+                      }
+                      20 = TEXT
+                      20 {
+                        field = tx_org_staff.contact_phone
+                        required = 1
+                      }
+                    }
+                      // contact_fax
+                    50 = COA
+                    50 {
+                      if {
+                        isTrue {
                           field = tx_org_staff.contact_fax
-                          required = 1
                         }
                       }
-                        // contact_skype
-                      60 = COA
-                      60 {
-                        if {
-                          isTrue {
-                            field = tx_org_staff.contact_skype
-                          }
+                      wrap = <li class="contact_fax">|</li>
+                      10 = TEXT
+                      10 {
+                        value = fax
+                        lang {
+                          de = Fax
+                          en = fax
                         }
-                        wrap = <li class="contact_skype">|</li>
-                        10 = TEXT
-                        10 {
-                          value = Skype
-                          lang {
-                            de = Skype
-                            en = Skype
-                          }
-                          noTrimWrap = ||: |
-                        }
-                        20 = TEXT
-                        20 {
+                        noTrimWrap = ||: |
+                      }
+                      20 = TEXT
+                      20 {
+                        field = tx_org_staff.contact_fax
+                        required = 1
+                      }
+                    }
+                      // contact_skype
+                    60 = COA
+                    60 {
+                      if {
+                        isTrue {
                           field = tx_org_staff.contact_skype
-                          required = 1
                         }
                       }
-                        // contact_url
-                      70 = COA
-                      70 {
-                        if {
-                          isTrue {
-                            field = tx_org_staff.contact_url
-                          }
+                      wrap = <li class="contact_skype">|</li>
+                      10 = TEXT
+                      10 {
+                        value = Skype
+                        lang {
+                          de = Skype
+                          en = Skype
                         }
-                        wrap = <li class="contact_url">|</li>
-                        10 = TEXT
-                        10 {
-                          value = Homepage
-                          lang {
-                            de = Homepage
-                            en = Homepage
-                          }
-                          noTrimWrap = || &raquo;|
-                          required = 1
-                          typolink {
-                            parameter{
-                              field = tx_org_staff.contact_url
-                            }
+                        noTrimWrap = ||: |
+                      }
+                      20 = TEXT
+                      20 {
+                        field = tx_org_staff.contact_skype
+                        required = 1
+                      }
+                    }
+                      // contact_url
+                    70 = COA
+                    70 {
+                      if {
+                        isTrue {
+                          field = tx_org_staff.contact_url
+                        }
+                      }
+                      wrap = <li class="contact_url">|</li>
+                      10 = TEXT
+                      10 {
+                        value = Homepage
+                        lang {
+                          de = Homepage
+                          en = Homepage
+                        }
+                        noTrimWrap = || &raquo;|
+                        required = 1
+                        typolink {
+                          parameter{
+                            field = tx_org_staff.contact_url
                           }
                         }
                       }
@@ -215,68 +240,64 @@ plugin.tx_browser_pi1 {
                     }
                       // div box
                     wrap = <ul class="vcard tx_org_headquarters">|</ul><!-- vcard -->
-                      // Marginal headquarters box: header, items
-                    10 = COA
+                      // header
+                    10 = TEXT
                     10 {
-                        // header
-                      10 = TEXT
-                      10 {
-                        value = Units
-                        lang {
-                          de = Bereiche
-                          en = Units
-                        }
-                        wrap = <li class="header">|</li>
+                      value = Units
+                      lang {
+                        de = Bereiche
+                        en = Units
                       }
-                        // items: tx_org_headquarters.title croped and linked
-                      20 = CONTENT
-                      20 {
-                        table = tx_org_headquarters
-                        select {
-                          pidInList = {$plugin.org.sysfolder.headquarters}
-                          //selectFields = tx_org_headquarters.title
-                          join = tx_org_mm_all ON tx_org_mm_all.uid_local = tx_org_headquarters.uid
-                          where {
-                            field = tx_org_staff.uid
-                            noTrimWrap = |tx_org_mm_all.uid_foreign = | AND tx_org_mm_all.table_foreign = 'tx_org_staff' AND tx_org_mm_all.table_local = 'tx_org_headquarters'|
-                          }
-                          orderBy = tx_org_headquarters.title
-                          max = 3
+                      wrap = <li class="header">|</li>
+                    }
+                      // items: tx_org_headquarters.title croped and linked
+                    20 = CONTENT
+                    20 {
+                      table = tx_org_headquarters
+                      select {
+                        pidInList = {$plugin.org.sysfolder.headquarters}
+                        //selectFields = tx_org_headquarters.title
+                        join = tx_org_mm_all ON tx_org_mm_all.uid_local = tx_org_headquarters.uid
+                        where {
+                          field = tx_org_staff.uid
+                          noTrimWrap = |tx_org_mm_all.uid_foreign = | AND tx_org_mm_all.table_foreign = 'tx_org_staff' AND tx_org_mm_all.table_local = 'tx_org_headquarters'|
                         }
-                          // tx_org_headquarters.title croped and linked
-                        renderObj = CASE
-                        renderObj {
-                          key {
-                            field = {$plugin.tx_browser_pi1.templates.listview.url.2.key}
+                        orderBy = tx_org_headquarters.title
+                        max = 3
+                      }
+                        // tx_org_headquarters.title croped and linked
+                      renderObj = CASE
+                      renderObj {
+                        key {
+                          field = {$plugin.tx_browser_pi1.templates.listview.url.2.key}
+                        }
+                          // link to detail view
+                        default = TEXT
+                        default {
+                          field = marginal_title // teaser_title // title
+                          //crop = 24 | ... &raquo; | 1
+                          wrap = <li class="url circle">|</li>
+                          stdWrap {
+                            noTrimWrap = || &raquo;|
                           }
-                            // link to detail view
-                          default = TEXT
-                          default {
-                            field = marginal_title // teaser_title // title
-                            //crop = 24 | ... &raquo; | 1
-                            wrap = <li class="url circle">|</li>
-                            stdWrap {
-                              noTrimWrap = || &raquo;|
-                            }
-                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.default
-                          }
-                            // no link
-                          notype = TEXT
-                          notype {
-                            field   = title
-                            crop    = 24 | ... | 1
-                            stdWrap >
-                          }
-                            // link to internal page
-                          page < .default
-                          page {
-                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.page
-                          }
-                            // link to external url
-                          url < .default
-                          url {
-                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.url
-                          }
+                          typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.default
+                        }
+                          // no link
+                        notype = TEXT
+                        notype {
+                          field   = title
+                          crop    = 24 | ... | 1
+                          stdWrap >
+                        }
+                          // link to internal page
+                        page < .default
+                        page {
+                          typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.page
+                        }
+                          // link to external url
+                        url < .default
+                        url {
+                          typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.2.url
                         }
                       }
                     }
