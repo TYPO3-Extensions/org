9184202bd4279c394f2601fde6ae91f116698457
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-28 13:13:23 +0000

    6.0.7 +++ 201 one column
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@87881 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/staff/101/tx_browser_pi1/single/onecolumn/htmlSnippets.ts
@@ -0,0 +1,35 @@
+plugin.tx_browser_pi1 {
+  views {
+    single {
+      102 {
+        htmlSnippets =
+        htmlSnippets {
+          subparts {
+            singleview = TEXT
+            singleview {
+              value (
+                <!-- ###SINGLEVIEW### begin --><!-- ###SINGLEBODY### begin --><!-- ###SINGLEBODYROW### begin -->
+                <!-- ###AREA_FOR_AJAX_LIST_01### begin -->
+                <div class="main">
+                  ###RECORD_BROWSER###
+                  ###TX_ORG_STAFF.TITLE###
+                  ###TX_ORG_STAFF.DELETED###<!-- tx_org_staffgroup -->
+                  ###TX_ORG_STAFF.CRDATE###<!-- tx_org_news -->
+                  <!-- ###AREA_FOR_AJAX_LIST_01### end -->
+                  <!-- ###AREA_FOR_AJAX_LIST_02### begin -->
+                  <!-- ###BACKBUTTON### begin -->
+                  <p class="backbutton">
+                    ###MY_SINGLEVIEWBACKBUTTON###
+                  </p>
+                  <!-- ###BACKBUTTON### end -->
+                </div>
+                <!-- ###AREA_FOR_AJAX_LIST_02### end -->
+                <!-- ###SINGLEBODYROW### end --><!-- ###SINGLEBODY### end --><!-- ###SINGLEVIEW### end -->
+)
+            }
+          }
+        }
+      }
+    }
+  }
+}
