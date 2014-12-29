d38c57d09aed106a8cd1e382f187013b9be1a8ca
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-29 19:41:15 +0000

    6.0.7 +++ 701
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@87908 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/location/701/tx_browser_pi1/single/marker.ts
+++ Configuration/TypoScript/location/701/tx_browser_pi1/single/marker.ts
@@ -3,13 +3,13 @@ plugin.tx_browser_pi1 {
     my_singleviewbackbutton = COA
     my_singleviewbackbutton {
       30 {
-        value = All companies
+        value = All locations
         lang {
-          de = Alle Firmen
-          en = All companies
+          de = Alle Veranstaltungsorte
+          en = All locations
         }
         typolink {
-          parameter = {$plugin.org.pages.headquarters}
+          parameter = {$plugin.org.pages.location}
         }
       }
     }
