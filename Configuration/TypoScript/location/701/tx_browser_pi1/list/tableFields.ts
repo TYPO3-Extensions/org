05df8099cad68a8370e1e4a7a0a653cb8ce257a1
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-12-29 19:59:15 +0000

    6.0.7 +++ 701
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@87911 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/location/701/tx_browser_pi1/list/tableFields.ts
+++ Configuration/TypoScript/location/701/tx_browser_pi1/list/tableFields.ts
@@ -19,7 +19,7 @@ plugin.tx_browser_pi1 {
                 10 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.image.0
                 wrap = <div>|</div>
               }
-              wrap = <div class="show-for-large-up columns small-12 medium-12 large-4">|</div>
+              wrap = <div class="show-for-large-up columns large-4">|</div>
             }
               // bookmarks, title, subline
             20 = COA
@@ -81,9 +81,8 @@ plugin.tx_browser_pi1 {
                   // link to the single view (record), internal page, external URL or no link (empty value)
                 30 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.details.0
               }
-              wrap = <div class="columns small-12 medium-12 large-8">|</div>
+              wrap = <div class="columns large-8">|</div>
             }
-            wrap = <div class="row">|</div>
           }
         }
       }
