e0ed373f6c609c63b931e8672cb44e03f52ec8d4
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-02 01:55:53 +0000

    6.0.7 +++ marginal constants
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@88032 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/base/plugins/tx_browser_pi1/constants.ts
+++ Configuration/TypoScript/base/plugins/tx_browser_pi1/constants.ts
@@ -1,6 +1,87 @@
 plugin.tx_browser_pi1 {
   templates {
     listview {
+      header {
+        1 {
+          field   = tx_org_staff.name_last
+          tag     = h4
+        }
+        2 {
+          field   = tx_org_headquarters.teaser_title // tx_org_headquarters.title
+          tag     = h4
+        }
+        3 {
+          field   = tx_org_news.marginal_title // tx_org_news.teaser_title // tx_org_news.title
+          tag     = h4
+        }
+        4 {
+          field   = tx_org_location.teaser_title // tx_org_location.title
+          tag     = h4
+        }
+        5 {
+          field   = tx_org_cal.teaser_title // tx_org_cal.title
+          tag     = h4
+        }
+      }
+      image {
+        1 {
+          default   = EXT:org/res/icons/defaults/tx_org_staff_300x200.png
+          file      = tx_org_staff.image
+          height    = 56c
+          path      = uploads/tx_org/
+          seo       = tx_org_staff.imageseo
+          width     = 56c
+        }
+        2 {
+          default   = EXT:org/res/icons/defaults/tx_org_headquarters_300x200.png
+          file      = tx_org_headquarters.image
+          height    = 90c
+          path      = uploads/tx_org/
+          seo       = tx_org_headquarters.imageseo
+          width     = 220c
+        }
+        3 {
+          default   = EXT:org/res/icons/defaults/tx_org_news_300x200.png
+          file      = tx_org_news.image
+          height    = 90c
+          path      = uploads/tx_org/
+          seo       = tx_org_news.imageseo
+          width     = 220c
+        }
+        4 {
+          default   = EXT:org/res/icons/defaults/tx_org_location_300x200.png
+          file      = tx_org_location.image
+          height    = 90c
+          path      = uploads/tx_org/
+          seo       = tx_org_location.imageseo
+          width     = 220c
+        }
+        5 {
+          default   = EXT:org/res/icons/defaults/tx_org_cal_300x200.png
+          file      = tx_org_cal.image
+          height    = 90c
+          path      = uploads/tx_org/
+          seo       = tx_org_cal.imageseo
+          width     = 220c
+        }
+      }
+      text {
+        1 {
+          field   = tx_org_staff.bodytext // tx_org_staff.vita
+        }
+        2 {
+          field   = tx_org_headquarters.teaser_short // tx_org_headquarters.bodytext
+        }
+        3 {
+          field   = tx_org_news.teaser_short // tx_org_news.bodytext
+        }
+        4 {
+          field   = tx_org_location.teaser_short // tx_org_location.bodytext
+        }
+        5 {
+          field   = tx_org_cal.teaser_short // tx_org_cal.bodytext
+        }
+      }
       url {
         1 {
           key       = tx_org_staff.type // type
