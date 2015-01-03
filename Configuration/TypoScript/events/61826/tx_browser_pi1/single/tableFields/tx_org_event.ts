f026837bb25b945b07f971452a461cd57f8e2926
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 14:05:32 +0000

    6.0.7 +++ 201 event single
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@88102 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/events/61826/tx_browser_pi1/single/tableFields/tx_org_event.ts
+++ Configuration/TypoScript/events/61826/tx_browser_pi1/single/tableFields/tx_org_event.ts
@@ -5,101 +5,92 @@ plugin.tx_browser_pi1 {
           // 140706: empty statement: for proper comments only
         tx_org_event {
         }
-          // title: image, header, bodytext, vita, groups
+          // title: image, header, bodytext
         tx_org_event =
         tx_org_event {
-            // image, header, bodytext, vita, groups
-          title = COA
-          title {
-              // socialmedia_bookmarks
-            10 = TEXT
-            10 {
-              value = ###SOCIALMEDIA_BOOKMARKS###
-              wrap = <div class="show-for-large-up socialbookmarks">|</div>
-            }
-              // image
-            11 = COA
-            11 {
-              if {
-                isTrue {
-                  field = tx_org_event.image
-                }
-              }
-              wrap = <div style="float:left;padding:0 1em 1em 0;">|</div>
-              10 < plugin.tx_browser_pi1.displaySingle.master_templates.tableFields.image.0.notype
-            }
-              // header
-            20 = TEXT
+          title < plugin.tx_browser_pi1.displaySingle.master_templates.tableFields.imageText.0
+          XXXtitle {
             20 {
-              field = tx_org_event.title
-              wrap = <h1>|</h1>
-            }
-              // bodytext
-            30 = TEXT
-            30 {
-              field = tx_org_event.bodytext
-              stdWrap {
-                parseFunc < lib.parseFunc_RTE
-              }
-            }
-              // tx_org_eventcat.title
-            90 = COA
-            90 {
-                // Marginal news box: header, items
-              10 = COA
-              10 {
-                  // header
-                10 = TEXT
+              0 {
+                10 >
+                  // bookmarks, header, bodytext
+                10 = COA
                 10 {
-                  value = Groups
-                  lang {
-                    de = Gruppen
-                    en = Groups
+                    // socialmedia_bookmarks
+                  10 = TEXT
+                  10 {
+                    value = ###SOCIALMEDIA_BOOKMARKS###
+                    wrap = <div class="show-for-large-up socialbookmarks">|</div>
                   }
-                  noTrimWrap = |<span class="header">|:</span> |
-                }
-                  // items: tx_org_eventcat.title croped and linked
-                20 = CONTENT
-                20 {
-                  table = tx_org_eventcat
-                  select {
-                    pidInList = {$plugin.org.sysfolder.headquarters}
-                    //selectFields = tx_org_eventcat.title
-                    join = tx_org_mm_all ON tx_org_mm_all.uid_foreign = tx_org_eventcat.uid
-                    where {
-                      field = tx_org_event.uid
-                      noTrimWrap = |tx_org_mm_all.uid_local = | AND tx_org_mm_all.table_local = 'tx_org_event' AND tx_org_mm_all.table_foreign = 'tx_org_eventcat'|
+                    // header
+                  20 = TEXT
+                  20 {
+                    field = tx_org_event.title // tx_org_cal.title
+                    wrap = <h1>|</h1>
+                  }
+                    // tx_org_cal.subtitle
+                  21 = TEXT
+                  21 {
+                    field = tx_org_event.subtitle // tx_org_cal.subtitle
+                    wrap = <h2>|</h2>
+                    required = 1
+                  }
+                    // bodytext
+                  30 = TEXT
+                  30 {
+                    field = tx_org_event.bodytext // tx_org_cal.bodytext
+                    required = 1
+                    stdWrap {
+                      parseFunc < lib.parseFunc_RTE
                     }
-                    orderBy = tx_org_eventcat.title
-                    max = 3
                   }
-                    // tx_org_eventcat.title
-                  renderObj = TEXT
-                  renderObj {
-                    field = title
-                    wrap = |###POINT###
-                  }
-                  stdWrap {
-                    split {
-                      token = ###POINT###
-                      cObjNum = 1 |*| 1 |*| 2 || 2
-                      1.current = 1
-                      1.noTrimWrap = ||, |
-                      2.current = 1
-                      2.noTrimWrap = |||
+                    // length
+                  31 = TEXT
+                  31 {
+                    field = tx_org_event.length
+                    required = 1
+                    stdWrap {
+                      parseFunc < lib.parseFunc_RTE
                     }
                   }
                 }
               }
-                // if is true tx_org_eventcat.uid
-              if =
-              if {
-                isTrue {
-                  field = tx_org_eventcat.uid
-                }
+              1 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
               }
-                // div box
-              wrap = <div class="show-for-large-up tx_org_eventcat">|</div>
+              2 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              8 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              9 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              10 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              17 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              18 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              25 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
+              26 {
+                10 >
+                10 < plugin.tx_browser_pi1.views.single.201.tx_org_cal.title.20.0.10
+              }
             }
           }
         }
