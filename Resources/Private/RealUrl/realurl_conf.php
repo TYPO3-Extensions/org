7c8b3348028fa0ce4eac4c80105a384bb8172ab2
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 13:50:24 +0000

    6.0.7 +++ realurl
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@88098 735d13b6-9817-0410-8766-e36946ffe9aa

--- Resources/Private/RealUrl/realurl_conf.php
+++ Resources/Private/RealUrl/realurl_conf.php
@@ -8,6 +8,17 @@
   // typo3conf/realurl_conf.php
 
 
+
+// http://wiki.typo3.org/RealURL
+ $rootPids = array(
+     'hfs.typo3-org.de' => 1,
+     'hfs-berlin.de' => 1,
+     'www.hfs-berlin.de' => 1,
+     'bat.typo3-org.de' => 5, 
+     'bat-berlin.de' => 5, 
+     'www.bat-berlin.de' => 5, 
+ ); 
+
   //////////////////////////////////
   //
   // Default real URL configuration
@@ -32,7 +43,7 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl'] = array
       'spaceCharacter'    => '-',
       'languageGetVar'    => 'L',
       'expireDays'        => 7,
-      'rootpage_id'       => 0,
+      'rootpage_id'       => $rootPids[$_SERVER['HTTP_HOST']],
       'firstHitPathCache' => 1,
     ),
     'preVars' => array
@@ -42,9 +53,24 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl'] = array
         'GETvar'    => 'no_cache',
         'valueMap'  => array
         (
-          'nc' => 1,
+          //'nc' => 1, // removed because of cookies and treeview
         ),
         'noMatch' => 'bypass',
+      ),
+    ),
+    // configure filenames for different pagetypes
+    'fileName' => array(
+      'index' => array(
+        'druck.html' => array(
+          'keyValues' => array(
+            'type' => 98,
+          ),
+        ),
+        'pdf.html' => array(
+          'keyValues' => array(
+            'type' => 123,
+          ),
+        ),
       ),
     ),
   ),
@@ -59,15 +85,15 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets'] = array
 (
   '_DEFAULT' => array 
   (
-    // news: organiser_news
-    'news' => array 
+    // department: tx_org_department
+    'abteilung' => array 
     (
       array
       (
-        'GETvar' => 'tx_browser_pi1[showUid]',
+        'GETvar' => 'tx_browser_pi1[departmentUid]',
         'lookUpTable' => array
         (
-          'table'               => 'tx_organiser_news',
+          'table'               => 'tx_org_department',
           'id_field'            => 'uid',
           'alias_field'         => 'title',
           'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
@@ -80,15 +106,166 @@ $TYPO3_CONF_VARS['EXTCONF']['realurl']['_DEFAULT']['postVarSets'] = array
         )
       ),
     ),
-    'browse' => array 
+    // datetime: tx_org_cal
+    'zeitraum' => array 
+    (
+      array
+      (
+        'GETvar' => 'tx_browser_pi1[tx_org_cal.datetime]',
+      ),
+    ),
+    // calendar: tx_org_cal
+    'termin' => array 
+    (
+      array
+      (
+        'GETvar' => 'tx_browser_pi1[calendarUid]',
+        'lookUpTable' => array
+        (
+          'table'               => 'tx_org_cal',
+          'id_field'            => 'uid',
+          'alias_field'         => 'title',
+          'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
+          'useUniqueCache'      => 1,
+          'useUniqueCache_conf' => array
+          (
+            'strtolower'      => 1,
+            'spaceCharacter'  => '-',
+          ),
+        )
+      ),
+    ),
+    // news: tx_org_news
+    'nachricht' => array 
+    (
+      array
+      (
+        'GETvar' => 'tx_browser_pi1[newsUid]',
+        'lookUpTable' => array
+        (
+          'table'               => 'tx_org_news',
+          'id_field'            => 'uid',
+          'alias_field'         => 'title',
+          'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
+          'useUniqueCache'      => 1,
+          'useUniqueCache_conf' => array
+          (
+            'strtolower'      => 1,
+            'spaceCharacter'  => '-',
+          ),
+        )
+      ),
+    ),
+    // staff: fe_users
+    'name' => array 
+    (
+      array
+      (
+        'GETvar' => 'tx_browser_pi1[staffUid]',
+        'lookUpTable' => array
+        (
+          'table'               => 'tx_org_staff',
+          'id_field'            => 'uid',
+          'alias_field'         => 'title',
+          'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
+          'useUniqueCache'      => 1,
+          'useUniqueCache_conf' => array
+          (
+            'strtolower'      => 1,
+            'spaceCharacter'  => '-',
+          ),
+        )
+      ),
+    ),
+    // headquarter: tx_org_headquarters
+    'bereich' => array 
+    (
+      array
+      (
+        'GETvar' => 'tx_browser_pi1[headquartersUid]',
+        'lookUpTable' => array
+        (
+          'table'               => 'tx_org_headquarters',
+          'id_field'            => 'uid',
+          'alias_field'         => 'title',
+          'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
+          'useUniqueCache'      => 1,
+          'useUniqueCache_conf' => array
+          (
+            'strtolower'      => 1,
+            'spaceCharacter'  => '-',
+          ),
+        )
+      ),
+    ),
+    // repertoire: tx_org_repertoire
+    'stueck' => array 
+    (
+      array
+      (
+        'GETvar' => 'tx_browser_pi1[repertoireUid]',
+        'lookUpTable' => array
+        (
+          'table'               => 'tx_org_repertoire',
+          'id_field'            => 'uid',
+          'alias_field'         => 'title',
+          'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
+          'useUniqueCache'      => 1,
+          'useUniqueCache_conf' => array
+          (
+            'strtolower'      => 1,
+            'spaceCharacter'  => '-',
+          ),
+        )
+      ),
+    ),
+    // veranstalter: tx_org_location
+    'veranstalter' => array 
+    (
+      array
+      (
+        'GETvar' => 'tx_browser_pi1[locationUid]',
+        'lookUpTable' => array
+        (
+          'table'               => 'tx_org_location',
+          'id_field'            => 'uid',
+          'alias_field'         => 'title',
+          'addWhereClause'      => ' AND NOT deleted AND NOT hidden',
+          'useUniqueCache'      => 1,
+          'useUniqueCache_conf' => array
+          (
+            'strtolower'      => 1,
+            'spaceCharacter'  => '-',
+          ),
+        )
+      ),
+    ),
+    'ansicht' => array 
+    (
+      array 
+      (
+        'GETvar' => 'tx_browser_pi1[mode]',
+      ),
+    ),
+    'az' => array 
     (
       array 
       (
         'GETvar' => 'tx_browser_pi1[azTab]',
       ),
+    ),
+    'seite' => array 
+    (
       array 
       (
         'GETvar' => 'tx_browser_pi1[pointer]',
+      ),
+    ),
+    'pl' => array 
+    (    
+      array 
+      (
+        'GETvar' => 'tx_browser_pi1[plugin]',
       ),
     ),
     'sort' => array 
