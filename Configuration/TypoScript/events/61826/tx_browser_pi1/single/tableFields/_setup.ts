83e2b7fd1d2b698370d9ff71275e7a81c97f1a6b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-03 15:37:00 +0000

    6.0.7 +++ event single
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@88116 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/events/61826/tx_browser_pi1/single/tableFields/_setup.ts
+++ Configuration/TypoScript/events/61826/tx_browser_pi1/single/tableFields/_setup.ts
@@ -1,1 +1,2 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:org/Configuration/TypoScript/events/61826/tx_browser_pi1/single/tableFields/tx_org_event.ts">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org/Configuration/TypoScript/events/61826/tx_browser_pi1/single/tableFields/tx_org_cal.ts">
