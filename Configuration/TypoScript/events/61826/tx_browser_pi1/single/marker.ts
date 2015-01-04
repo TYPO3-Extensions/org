6c76f7ccb603e42a724e5ff618680ea70a0c5c7a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-01-04 18:39:25 +0000

    6.0.7 +++ event +marker
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@88143 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/events/61826/tx_browser_pi1/single/marker.ts
@@ -0,0 +1,16 @@
+plugin.tx_browser_pi1 {
+  marker {
+    my_singleviewbackbutton {
+      30 {
+        value = Events
+        lang {
+          de = Veranstaltungen
+          en = Events
+        }
+        typolink {
+          parameter = {$plugin.org.pages.event}
+        }
+      }
+    }
+  }
+}
