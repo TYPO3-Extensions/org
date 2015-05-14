a57523671db2ea6e1e6a8440ef5f5f6c2dae9240
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-05-14 23:09:58 +0000

    7.1.1 +++ #i0067
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89129 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/news/412/tx_browser_pi1/list/tableFields.ts
+++ Configuration/TypoScript/news/412/tx_browser_pi1/list/tableFields.ts
@@ -6,45 +6,27 @@ plugin.tx_browser_pi1 {
           title = COA
           title {
             10 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.header.3
-            20 = TEXT
-            20 {
-              field = tx_org_news.marginal_subtitle // tx_org_news.teaser_subtitle // tx_org_news.subtitle
-              required = 1
-              noTrimWrap = |<p>|</p> |
-              stdWrap {
-                crop = 80|...|1
-              }
-            }
-            20 >
-            30 < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.3
-            30 {
+            10 {
               default {
-                10 >
-                20 {
-                  value >
-                  field < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.3.default.10.field
+                stdWrap {
+                  wrap = |&nbsp;&raquo;
                 }
+                wrap = <p class="black-on-white">|</p>
               }
               notype {
-                10 >
-                20 {
-                  value >
-                  field < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.3.default.10.field
-                }
+                wrap = <p class="black-on-white">|</p>
               }
               page {
-                10 >
-                20 {
-                  value >
-                  field < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.3.default.10.field
+                stdWrap {
+                  wrap = |&nbsp;&raquo;
                 }
+                wrap = <p class="black-on-white">|</p>
               }
               url {
-                10 >
-                20 {
-                  value >
-                  field < plugin.tx_browser_pi1.displayList.master_templates.tableFields.text.3.default.10.field
+                stdWrap {
+                  wrap = |&nbsp;&raquo;
                 }
+                wrap = <p class="black-on-white">|</p>
               }
             }
             wrap = <div class="record">|</div>
