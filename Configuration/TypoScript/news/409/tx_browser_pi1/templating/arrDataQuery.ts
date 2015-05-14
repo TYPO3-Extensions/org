e86d2b30d0b6e6c869f9d8799529dfbd7d14b240
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-05-14 11:29:04 +0000

    7.1.1 +++ #i0063/64
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89108 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/news/409/tx_browser_pi1/templating/arrDataQuery.ts
+++ Configuration/TypoScript/news/409/tx_browser_pi1/templating/arrDataQuery.ts
@@ -3,7 +3,7 @@ plugin.tx_browser_pi1 {
     templating {
       arrDataQuery {
         items {
-          409 {
+          1 {
             name = Top News
             arrQuery {
               andWhere = tx_org_news.topnews = 1
