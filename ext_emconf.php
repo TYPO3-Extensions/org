154568f538b9b92ec99df97b81be42e593848f14
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-05-31 20:59:00 +0000

    7.2.0 +++ #67210
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89193 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
   ,
   'category' => 'plugin',
   'shy' => 0,
-  'version' => '7.1.0',
+  'version' => '7.2.0',
   'conflicts' => 'css_filelinks',
   'priority' => '',
   'loadOrder' => '',
