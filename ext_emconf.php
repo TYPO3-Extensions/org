fc264ebf474f2ab634656d73b2e008fbbf30cc01
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-06-01 20:22:38 +0000

    7.2.1 +++ Cleanup vCard constants
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89205 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
   ,
   'category' => 'plugin',
   'shy' => 0,
-  'version' => '7.2.0',
+  'version' => '7.2.1',
   'conflicts' => 'css_filelinks',
   'priority' => '',
   'loadOrder' => '',
