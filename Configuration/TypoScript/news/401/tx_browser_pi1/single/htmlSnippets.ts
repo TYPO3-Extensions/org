26365b046f7845752b985d6855da7aa5438e2368
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-09-06 10:07:27 +0000

    7.5.0 +++ PDF Controller
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89702 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/news/401/tx_browser_pi1/single/htmlSnippets.ts
+++ Configuration/TypoScript/news/401/tx_browser_pi1/single/htmlSnippets.ts
@@ -29,3 +29,38 @@ plugin.tx_browser_pi1 {
     }
   }
 }
+
+#i0077
+[globalVar = GP:type = {$plugin.pdfcontroller.pages.print.typeNum}]
+  plugin.tx_browser_pi1 {
+    views {
+      single {
+        401 {
+          htmlSnippets =
+          htmlSnippets {
+            subparts {
+              singleview = TEXT
+              singleview {
+                value (
+                  <!-- ###SINGLEVIEW### begin --><!-- ###SINGLEBODY### begin --><!-- ###SINGLEBODYROW### begin -->
+                  <!-- ###AREA_FOR_AJAX_LIST_01### begin -->
+                  <div class="main columns small-12 medium-12 large-8">
+                    ###RECORD_BROWSER###
+                    ###TX_ORG_NEWS.TITLE###
+                  </div>
+                  <div class="margin columns show-for-large-up small-12 medium-12 large-4">
+                    ###TX_ORG_STAFF.UID###
+                    ###TX_ORG_NEWS.CRDATE###
+                    ###TX_ORG_NEWS.DELETED###
+                  </div>
+                  <!-- ###AREA_FOR_AJAX_LIST_02### end -->
+                  <!-- ###SINGLEBODYROW### end --><!-- ###SINGLEBODY### end --><!-- ###SINGLEVIEW### end -->
+)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+[global]
