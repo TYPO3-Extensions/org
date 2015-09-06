b98b28b906c2715437c8cca1b7e333cf7c811d45
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-09-06 13:36:24 +0000

    7.5.0 +++ PDF Controller
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89712 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/news/401/401_newsonly/single/htmlSnippets.ts
+++ Configuration/TypoScript/news/401/401_newsonly/single/htmlSnippets.ts
@@ -31,3 +31,34 @@ plugin.tx_browser_pi1 {
     }
   }
 }
+
+#i0077
+[globalVar = GP:type = {$plugin.pdfcontroller.pages.print.typeNum}]
+  plugin.tx_browser_pi1 {
+    views {
+      single {
+        401 {
+          htmlSnippets {
+            subparts {
+              singleview {
+                value (
+                  <!-- ###SINGLEVIEW### begin --><!-- ###SINGLEBODY### begin --><!-- ###SINGLEBODYROW### begin -->
+                  <!-- ###AREA_FOR_AJAX_LIST_01### begin -->
+                  <table class="main">
+                    <tbody>
+                      <tr>
+                        ###TX_ORG_NEWS.TITLE###
+                      </tr>
+                    </tbody>
+                  </table>
+                  <!-- ###AREA_FOR_AJAX_LIST_02### end -->
+                  <!-- ###SINGLEBODYROW### end --><!-- ###SINGLEBODY### end --><!-- ###SINGLEVIEW### end -->
+)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+[global]
