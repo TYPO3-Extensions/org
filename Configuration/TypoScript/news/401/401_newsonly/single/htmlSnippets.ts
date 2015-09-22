436637353280be70bfaf5b19c7d4ef80b971aec3
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-09-22 09:02:22 +0000

    7.5.2 +++ Backbutton News Single
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89824 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/news/401/401_newsonly/single/htmlSnippets.ts
+++ Configuration/TypoScript/news/401/401_newsonly/single/htmlSnippets.ts
@@ -14,11 +14,6 @@ plugin.tx_browser_pi1 {
                   ###RECORD_BROWSER###
                   ###TX_ORG_NEWS.TITLE###
                   <!-- ###AREA_FOR_AJAX_LIST_01### end -->
-                  <!-- ###BACKBUTTON### begin -->
-                  <p class="backbutton">
-                    ###MY_SINGLEVIEWBACKBUTTON###
-                  </p>
-                  <!-- ###BACKBUTTON### end -->
                   <!-- ###AREA_FOR_AJAX_LIST_02### begin -->
                 </div>
                 <!-- ###AREA_FOR_AJAX_LIST_02### end -->
