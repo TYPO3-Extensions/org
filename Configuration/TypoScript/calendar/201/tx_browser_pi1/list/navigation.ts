8d8c19fc294a5728f60e3b9b5ff5099fc16ba0f6
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-22 22:28:50 +0000

    7.5.4 +++ Calendar items got a date and a location in the pop up of the map
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89957 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/calendar/201/tx_browser_pi1/list/navigation.ts
+++ Configuration/TypoScript/calendar/201/tx_browser_pi1/list/navigation.ts
@@ -79,6 +79,176 @@ plugin.tx_browser_pi1 {
                     }
                       // text
                     40 {
+                      default {
+                        10 >
+                          // datetime, location
+                        10 = COA
+                        10 {
+                            // datetime
+                          10 = TEXT
+                          10 {
+                            field = tx_org_cal.datetime
+                            required  = 1
+                            strftime {
+                              stdWrap {
+                                cObject = TEXT
+                                cObject {
+                                  value = %m/%d/%y
+                                  lang {
+                                    de = %a. %d.%b.%y
+                                    en = %m/%d/%y
+                                  }
+                                }
+                              }
+                            }
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = ||<br />|
+                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.default
+                          }
+                            // location
+                          20 = TEXT
+                          20 {
+                            field = tx_org_location.mail_city
+                            required  = 1
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = |||
+                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.default
+                          }
+                        }
+                      }
+                      notype {
+                        10 >
+                          // datetime, location
+                        10 = COA
+                        10 {
+                            // datetime
+                          10 = TEXT
+                          10 {
+                            field = tx_org_cal.datetime
+                            required  = 1
+                            strftime {
+                              stdWrap {
+                                cObject = TEXT
+                                cObject {
+                                  value = %m/%d/%y
+                                  lang {
+                                    de = %a. %d.%b.%y
+                                    en = %m/%d/%y
+                                  }
+                                }
+                              }
+                            }
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = ||<br />|
+                          }
+                            // location
+                          20 = TEXT
+                          20 {
+                            field = tx_org_location.mail_city
+                            required  = 1
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = |||
+                          }
+                        }
+                      }
+                      page {
+                        10 >
+                          // datetime, location
+                        10 = COA
+                        10 {
+                            // datetime
+                          10 = TEXT
+                          10 {
+                            field = tx_org_cal.datetime
+                            required  = 1
+                            strftime {
+                              stdWrap {
+                                cObject = TEXT
+                                cObject {
+                                  value = %m/%d/%y
+                                  lang {
+                                    de = %a. %d.%b.%y
+                                    en = %m/%d/%y
+                                  }
+                                }
+                              }
+                            }
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = ||<br />|
+                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.page
+                          }
+                            // location
+                          20 = TEXT
+                          20 {
+                            field = tx_org_location.mail_city
+                            required  = 1
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = |||
+                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.page
+                          }
+                        }
+                      }
+                      url {
+                        10 >
+                          // datetime, location
+                        10 = COA
+                        10 {
+                            // datetime
+                          10 = TEXT
+                          10 {
+                            field = tx_org_cal.datetime
+                            required  = 1
+                            strftime {
+                              stdWrap {
+                                cObject = TEXT
+                                cObject {
+                                  value = %m/%d/%y
+                                  lang {
+                                    de = %a. %d.%b.%y
+                                    en = %m/%d/%y
+                                  }
+                                }
+                              }
+                            }
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = ||<br />|
+                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.url
+                          }
+                            // location
+                          20 = TEXT
+                          20 {
+                            field = tx_org_location.mail_city
+                            required  = 1
+                            stdWrap {
+                              stripHtml         = 1
+                              htmlSpecialChars  = 0
+                            }
+                            noTrimWrap = |||
+                            typolink < plugin.tx_browser_pi1.displayList.master_templates.tableFields.typolinks.0.url
+                          }
+                        }
+                      }
                         // link to tx_org_event
                       tx_org_event < .default
                       tx_org_event {
