f45dc1987222c4fe0d15f27ae574a3e2cf235a84
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-23 20:44:39 +0000

    7.5.5 +++ New CSS class ul.vcard li.expired
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@89980 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
   ,
   'category' => 'plugin',
   'shy' => 0,
-  'version' => '7.5.4',
+  'version' => '7.5.5',
   'conflicts' => 'css_filelinks',
   'priority' => '',
   'loadOrder' => '',
