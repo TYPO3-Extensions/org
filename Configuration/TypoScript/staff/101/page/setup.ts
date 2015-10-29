e25bd454c1581b3cb05eab353f8840d9175c4b3c
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-29 16:16:58 +0000

    7.6.0 +++ Improve jss
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@90087 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/staff/101/page/setup.ts
+++ Configuration/TypoScript/staff/101/page/setup.ts
@@ -11,16 +11,16 @@ page {
       value (
 
         /* EXT:org/Configuration/TypoScript/staff/101/page/setup.ts */
-        var labelTxOrgCalSlidedown  = "More &raquo;";
-        var labelTxOrgCalSlideup    = "Less &laquo;";
-        var lnDeTxOrgCalSlidedown   = "Mehr &raquo;";
-        var lnDeTxOrgCalSlideup     = "Weniger &laquo;";
+        var labelTxOrgStaffMore  = "More &raquo;";
+        var labelTxOrgStaffLess  = "Less &laquo;";
+        var lnDeTxOrgStaffMore   = "Mehr &raquo;";
+        var lnDeTxOrgStaffLess   = "Weniger &laquo;";
 
         var htmlLang = $( "html" ).attr( "lang" ).substr( 0, 2 );
 
         if( htmlLang === "de" ) {
-          labelTxOrgCalSlidedown  = lnDeTxOrgCalSlidedown;
-          labelTxOrgCalSlideup    = lnDeTxOrgCalSlideup;
+          labelTxOrgStaffMore  = lnDeTxOrgStaffMore;
+          labelTxOrgStaffLess  = lnDeTxOrgStaffLess;
         }
 
         $( document ).ready( function( )
@@ -28,18 +28,18 @@ page {
           if( $( "ul.tx_org_cal li.toggle" ).length > 0 )
           {
             $( ".tx_org_cal_toggle" ).show( );
-            $( ".tx_org_cal_toggle" ).html( labelTxOrgCalSlidedown );
+            $( ".tx_org_cal_toggle" ).html( labelTxOrgStaffMore );
           }
 
-          $( ".tx_org_cal_toggle" ).click(function() {
+          $( ".tx_org_cal_toggle" ).click( function() {
             $( "ul.tx_org_cal li.toggle" ).toggle( "slow", function() {
-              if( $( "ul.tx_org_cal li.toggle" ).css( "display" ) == "none" )
+              if( $( "ul.tx_org_cal li.toggle" ).css( "display" ) === "none" )
               {
-                $( ".tx_org_cal_toggle" ).html( labelTxOrgCalSlidedown ).blur();
+                $( ".tx_org_cal_toggle" ).html( labelTxOrgStaffMore ).blur();
               }
               else
               {
-                $( ".tx_org_cal_toggle" ).html( labelTxOrgCalSlideup ).blur();
+                $( ".tx_org_cal_toggle" ).html( labelTxOrgStaffLess ).blur();
               }
             });
           });
@@ -48,4 +48,3 @@ page {
     }
   }
 }
-
