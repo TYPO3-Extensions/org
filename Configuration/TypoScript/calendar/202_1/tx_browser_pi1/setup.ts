0ed11f161e408a7f4389ec0f62ceee5328a272fd
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-30 21:12:42 +0000

    7.6.0 +++ Feature: new cal templates
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@90136 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/calendar/202_1/tx_browser_pi1/setup.ts
@@ -0,0 +1,18 @@
+plugin.tx_browser_pi1 {
+
+  views {
+    list {
+      203 < plugin.tx_browser_pi1.views.list.201
+      203 = +Org: Calendar (without any map)
+      203 {
+        name    = +Org: Calendar (without any map)
+      }
+    }
+    single {
+      203 < plugin.tx_browser_pi1.views.single.201
+      203 = +Org: Calendar (without any map)
+    }
+  }
+}
+
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:org/Configuration/TypoScript/calendar/203/tx_browser_pi1/list/setup.ts">
