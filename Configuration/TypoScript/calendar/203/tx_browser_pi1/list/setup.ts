e89330f3dc88def00ddd010b8e1ecd365009ae1a
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-30 21:18:06 +0000

    7.6.0 +++ Feature: new cal templates
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@90138 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Configuration/TypoScript/calendar/203/tx_browser_pi1/list/setup.ts
@@ -0,0 +1,24 @@
+plugin.tx_browser_pi1 {
+  views {
+    list {
+      203 {
+        navigation < plugin.tx_browser_pi1.navigation
+        navigation {
+          map {
+            enabled = disabled
+          }
+        }
+      }
+    }
+    single {
+      203 {
+        navigation < plugin.tx_browser_pi1.navigation
+        navigation {
+          map {
+            enabled = disabled
+          }
+        }
+      }
+    }
+  }
+}
