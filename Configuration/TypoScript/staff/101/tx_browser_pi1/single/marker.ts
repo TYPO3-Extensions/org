30b0b01d4b8062c213bbed6dd9c53d0d41f18212
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2016-01-06 00:31:32 +0000

    7.8.1 +++ Improve labelling
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/org/trunk@90481 735d13b6-9817-0410-8766-e36946ffe9aa

--- Configuration/TypoScript/staff/101/tx_browser_pi1/single/marker.ts
+++ Configuration/TypoScript/staff/101/tx_browser_pi1/single/marker.ts
@@ -3,11 +3,6 @@ plugin.tx_browser_pi1 {
     my_singleviewbackbutton {
       30 {
         data = LLL:EXT:org/Resources/Private/Language/locallang_db.xml:url_phrase.staffall
-        XXXvalue = All people
-        XXXlang {
-          de = Alle Personen
-          en = All people
-        }
         typolink {
           parameter = {$plugin.org.pages.staff}
         }
--- Configuration/TypoScript/staff/102/tx_browser_pi1/single/marker.ts
+++ Configuration/TypoScript/staff/102/tx_browser_pi1/single/marker.ts
@@ -2,11 +2,7 @@ plugin.tx_browser_pi1 {
   marker {
     my_singleviewbackbutton {
       30 {
-        value = All people
-        lang {
-          de = Alle Personen
-          en = All people
-        }
+        data = LLL:EXT:org/Resources/Private/Language/locallang_db.xml:url_phrase.staffall
         typolink {
           parameter = {$plugin.org.pages.staff}
         }
